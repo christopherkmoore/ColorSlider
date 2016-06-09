55640bc5fba02db07f17013e008e5e90c32a5a36
christopherkmoore <christopherkmoore1@gmailcom>
2016-06-09 13:49:08 -0700

    first commit

--- ColorSliderMyOwn/ViewController.swift
+++ ColorSliderMyOwn/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // create outlets
+    @IBOutlet weak var colorView: UIView!
+    
+    
+    @IBOutlet weak var greenSlide: UISlider!
+    @IBOutlet weak var blueSlide: UISlider!
+    @IBOutlet weak var redSlide: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func changeSlideColor() {
+        
+        if self.blueSlide == nil {
+            return
+        }
+        
+        let r: Float = redSlide.value
+        let cgr: CGFloat = CGFloat(r)
+        let b: Float = blueSlide.value
+        let cgb: CGFloat = CGFloat(b)
+        let g: Float = greenSlide.value
+        let cgg: CGFloat = CGFloat(g)
+        
+        colorView.backgroundColor = UIColor(red: cgr, green: cgg, blue: cgb, alpha: 1)
     }
 
     override func didReceiveMemoryWarning() {
